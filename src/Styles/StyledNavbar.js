ece5bbbc8cd51bfb56c59a4064b36ee413fc98a2
Deepak Singh <deepak.kumar@hoolime.co>
2022-02-12 16:10:15 +0530

    Add styled component for navbar

---
+++ src/Styles/StyledNavbar.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Nav = styled.div`
+background-color: blue;
+display: flex;
+justify-content: space-between;
+align-items: center;
+height: 3rem;
+font-size: 1.5rem;
+padding: 0 1rem;
+position: fixed;
+width: 100vw;
+
+& ul{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+
+    & li{
+        padding: 0 0.1rem;
+
+        & a:link{
+            text-decoration: none;
+            color: white;
+        }
+    }
+}
+
+& button{
+    width: 3rem;
+    height: 3rem;
+    font-size: 1.9rem;
+    font-weight: 900;
+    background: red;
+    color: white;
+    /* border: 1px solid red; */
+    border: none;
+    border-radius: 50%;
+    cursor: pointer;
+
+    &:hover{
+        background: white;
+        color: red;
+    }
+}
+`
+
+export const LeftNav = styled.div`
+font-weight: 700;
+`
